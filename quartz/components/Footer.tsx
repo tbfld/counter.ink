7e9d59871164d6137eb2309e7d20711406e8ec77
Ted Byfield <tbfld@users.noreply.github.com>
2025-02-26 14:42:15 -0500

    Quartz sync: Feb 26, 2025, 2:42 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-© {year} Ted Byfield. If you want something, just ask nicely. 👍🏼
+<a href="https://counter.ink/index.xml">RSS here<a>. © {year} Ted Byfield. If you want something, ask nice. 👍🏼
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
