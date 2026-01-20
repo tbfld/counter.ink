8f1da36d93d24add2c8ba00cd92d251e7523f998
Ted Byfield <tbfld@users.noreply.github.com>
2026-01-20 16:17:50 -0500

    Quartz sync: Jan 20, 2026, 4:17 PM

---
+++ quartz/components/AllTags.tsx
@@ -0,0 +1,48 @@
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/backlinks.scss"
+import { resolveRelative } from "../util/path"
+import { classNames } from "../util/lang"
+
+const AllTags: QuartzComponent = ({
+  fileData,
+  allFiles,
+  displayClass,
+}: QuartzComponentProps) => {
+  // Collect all unique tags from all files
+  const allTags = new Set<string>()
+  allFiles.forEach((file) => {
+    if (file.frontmatter?.tags) {
+      const tags = Array.isArray(file.frontmatter.tags)
+        ? file.frontmatter.tags
+        : [file.frontmatter.tags]
+      tags.forEach((tag: string) => allTags.add(tag))
+    }
+  })
+
+  // Sort tags alphabetically
+  const sortedTags = Array.from(allTags).sort((a, b) => a.localeCompare(b))
+
+  if (sortedTags.length === 0) {
+    return null
+  }
+
+  return (
+    <div class={classNames(displayClass, "backlinks")}>
+      <h3>Tags</h3>
+      <div class="tag-cloud">
+        {sortedTags.map((tag, index) => (
+          <>
+            <a href={resolveRelative(fileData.slug!, `tags/${tag}` as any)} class="internal tag-link">
+              {tag}
+            </a>
+            {index < sortedTags.length - 1 && <span> * </span>}
+          </>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+AllTags.css = style
+
+export default (() => AllTags) satisfies QuartzComponentConstructor
